eb36bccd5cdf1fd9431350bea5fac67505ea6515
Matt Allan <matt@Matts-MacBook-Pro.local>
2019-04-05 09:31:18 -0700

    resetting commits test

---
+++ src/sales_data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+
+
+def load_dataframe(file):
+    """
+    Takes a SQL generated text or csv file and converts it to a pandas dataframe for regressi
+    """
+    df = pd.read_fwf('file')
+    df = df.reset_index()
+    df.columns = ['grand_total', 'create_time', 'close_time']
+    df = df[2:155924]
+
+    df = df.reset_index().drop('index', axis=1)
+    df['grand_total'] = df['grand_total'].astype(float)
+    df['create_time'] = pd.to_datetime(df['create_time'])
+
+    df = df.drop(labels='close_time', axis=1) 
+
+    return df
+
---
+++ src/weather_data_cleaning.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+def load_data(file):
+    df = pd.read_csv('file')
+    df = find_nan_columns(df)
+
+    # Rename relevant columns to more readable names
+    df.rename(columns = {'tmpf': 'temp', 'p01i': 'precip', 'dwpf': 'dew_temp', 'relh': 'humidity'})
+
+    df['clouds'] = [to_category(df['metar'][i], df['valid'][i].hour) for i in range(len(df))]
+
+
+def find_nan_columns(df):
+    """
+    Finds and drops columns with over 90% NaN values
+    
+    Input: pandas DataFrame
+    
+    Output: pandas DataFrame
+    """
+    cols_to_drop = []
+    for col in df.columns:
+        if (df[col].isna().sum() > (df.shape[0]*.9)):
+            cols_to_drop.append(col)
+    df.drop(labels=cols_to_drop, axis=1, inplace=True)
+    return df
+
+
+
+def to_category(raw_metar, hour):
+
+    """
+    Converts raw metar data into categories for the level of cloud cover.
+
+    Inputs: (string)    raw metar data
+            (int)       hour pulled from timestamp object
+    
+    Output: (string)      categorical term for cloud cover
+    """
+    if not raw_metar or type(raw_metar) != str:
+        return "Error! Metar data unavailable!"
+    else:
+
+        if any(elem in raw_metar for elem in ["SKC", "CLR"]):
+            return "sunny"
+        elif "FEW" in raw_metar:
+            return "mostly sunny"
+        elif "SCT" in raw_metar:
+            return "partly sunny"
+        elif "BKN" in raw_metar:
+            return "mostly cloudy"
+        elif "OVC" in raw_metar:
+            return "overcast"
+        else:
+            return "Oops!"
